6462422c8a6fcb3a2c5a315617ce2d3a959ed3fd
Adrian Chiriac <52508424+chiriacadrian@users.noreply.github.com>
2020-06-24 19:05:49 +0200

    Updates

---
+++ precache-manifest.192d3177fe48090e9a08177d97bfd0ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b472dfa70daa91d17baff26e9b1344",
+    "url": "/movie-info-point/index.html"
+  },
+  {
+    "revision": "d3596bbad53773965d21",
+    "url": "/movie-info-point/static/css/main.02046fad.chunk.css"
+  },
+  {
+    "revision": "87ccabebbe4b2c79a300",
+    "url": "/movie-info-point/static/js/2.351ca58b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-info-point/static/js/2.351ca58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3596bbad53773965d21",
+    "url": "/movie-info-point/static/js/main.a15c0f8b.chunk.js"
+  },
+  {
+    "revision": "c2810f8807180addfaad",
+    "url": "/movie-info-point/static/js/runtime-main.b182337a.js"
+  },
+  {
+    "revision": "3dfea5a13125028dc179dfbdac2b3b22",
+    "url": "/movie-info-point/static/media/TMDB.3dfea5a1.svg"
+  }
+]);
